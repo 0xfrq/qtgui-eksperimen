c4bfd4855f92d44e825eafb0c982b6ee75bf5e5e
fariq <tonykriss2323@gmail.com>
2025-10-15 23:08:31 +0700

    la

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -3,211 +3,119 @@
 #include <QHBoxLayout>
 #include <QWidget>
 #include <QLabel>
+#include <QGridLayout>
 
 MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent), currentButton(0), isContinuousRunning(false)
+    : QMainWindow(parent)
 {
-    setWindowTitle("Linux Command Runner");
-    resize(600, 400);
+    setWindowTitle("ROS Command Runner");
+    resize(800, 600);
     
     QWidget *centralWidget = new QWidget(this);
     setCentralWidget(centralWidget);
     QVBoxLayout *mainLayout = new QVBoxLayout(centralWidget);
     
-    QHBoxLayout *buttonLayout = new QHBoxLayout();
+    QGridLayout *buttonLayout = new QGridLayout();
     
-    button1 = new QPushButton("List Files (ls -la)", this);
-    button2 = new QPushButton("Show Date/Time (date)", this);
-    button3 = new QPushButton("Disk Usage (df -h)", this);
-    button4 = new QPushButton("Launch OP3 Manager", this);
+    createButton("OP3 Manager", "roslaunch", QStringList() << "op3_manager" << "op3_manager.launch");
+    createButton("Vision 2024", "rosrun", QStringList() << "v1_detection" << "vision_2024ssh.py");
+    createButton("Driver Baru", "rosrun", QStringList() << "kri2024" << "driver_baru.py");
+    createButton("Process Com2", "rosrun", QStringList() << "kri2024" << "process_com2.py");
+    createButton("Motion Control", "rostopic", QStringList() << "pub" << "/DEWO/MotionControl/MotionModule" << "std_msgs/String" << "data: 'action'");
+    createButton("Odometry", "rosrun", QStringList() << "kri2024" << "odometry.py");
+    createButton("Receiver8", "rosrun", QStringList() << "protocolgc" << "receiver8.py");
+    createButton("Com Receiver Shooter", "rosrun", QStringList() << "communication" << "com_receivershooter.py");
+    createButton("Com Receiver Nasional", "rosrun", QStringList() << "communication" << "com_receivernasional.py");
+    createButton("Com Sender Kickoff", "rosrun", QStringList() << "communication" << "com_senderkickoff.py");
+    createButton("Com Sender Nasional", "rosrun", QStringList() << "communication" << "com_sendernasional.py");
     
-    buttonLayout->addWidget(button1);
-    buttonLayout->addWidget(button2);
-    buttonLayout->addWidget(button3);
-    buttonLayout->addWidget(button4);
+    int row = 0, col = 0;
+    foreach(QPushButton *button, buttonProcessMap.keys()) {
+        buttonLayout->addWidget(button, row, col);
+        col++;
+        if (col > 3) {
+            col = 0;
+            row++;
+        }
+    }
     
     QLabel *outputLabel = new QLabel("Command Output:", this);
     
     outputDisplay = new QTextEdit(this);
     outputDisplay->setReadOnly(true);
-    outputDisplay->setPlaceholderText("Click a button to execute a command...");
+    outputDisplay->setPlaceholderText("Click a button to start/stop a command...");
     
     mainLayout->addLayout(buttonLayout);
     mainLayout->addWidget(outputLabel);
     mainLayout->addWidget(outputDisplay);
-    
-    process = new QProcess(this);
-    continuousProcess = new QProcess(this);
-    
-    connect(button1, SIGNAL(clicked()), this, SLOT(onButton1Clicked()));
-    connect(button2, SIGNAL(clicked()), this, SLOT(onButton2Clicked()));
-    connect(button3, SIGNAL(clicked()), this, SLOT(onButton3Clicked()));
-    connect(button4, SIGNAL(clicked()), this, SLOT(onButton4Clicked()));
-    
-    connect(process, SIGNAL(finished(int)), this, SLOT(onProcessFinished(int)));
-    connect(process, SIGNAL(error(QProcess::ProcessError)), this, SLOT(onProcessError(QProcess::ProcessError)));
-    
-    connect(continuousProcess, SIGNAL(readyReadStandardOutput()), this, SLOT(onContinuousProcessOutput()));
 }
 
 MainWindow::~MainWindow()
 {
-    if (process) {
+    foreach(QProcess *proc, buttonProcessMap.values()) {
+        if (proc && proc->state() == QProcess::Running) {
+            proc->kill();
+            proc->waitForFinished();
+        }
+        delete proc;
+    }
+}
+
+void MainWindow::createButton(const QString &label, const QString &command, const QStringList &args)
+{
+    QPushButton *button = new QPushButton(label, this);
+    QProcess *process = new QProcess(this);
+    
+    buttonProcessMap[button] = process;
+    buttonCommandMap[button] = command;
+    buttonArgsMap[button] = args;
+    
+    connect(button, SIGNAL(clicked()), this, SLOT(onButtonClicked()));
+    connect(process, SIGNAL(readyReadStandardOutput()), this, SLOT(onProcessOutput()));
+    connect(process, SIGNAL(readyReadStandardError()), this, SLOT(onProcessOutput()));
+}
+
+void MainWindow::onButtonClicked()
+{
+    QPushButton *button = qobject_cast<QPushButton*>(sender());
+    if (button) {
+        toggleProcess(button);
+    }
+}
+
+void MainWindow::toggleProcess(QPushButton *button)
+{
+    QProcess *process = buttonProcessMap[button];
+    
+    if (process->state() == QProcess::Running) {
         process->kill();
         process->waitForFinished();
-    }
-    if (continuousProcess) {
-        continuousProcess->kill();
-        continuousProcess->waitForFinished();
-    }
-}
-
-void MainWindow::onButton1Clicked()
-{
-    executeCommand("ls -la", button1);
-}
-
-void MainWindow::onButton2Clicked()
-{
-    executeCommand("date", button2);
-}
-
-void MainWindow::onButton3Clicked()
-{
-    executeCommand("df -h", button3);
-}
-
-void MainWindow::onButton4Clicked()
-{
-    if (isContinuousRunning) {
-        stopContinuousCommand();
-    } else {
-        startContinuousCommand();
-    }
-}
-
-void MainWindow::executeCommand(const QString &command, QPushButton *button)
-{
-    if (process->state() == QProcess::Running) {
-        outputDisplay->append("\n[ERROR] A command is already running. Please wait...\n");
-        return;
-    }
-    
-    currentButton = button;
-    setButtonState(button, "running");
-    
-    outputDisplay->clear();
-    outputDisplay->append(QString("Executing: %1\n").arg(command));
-    outputDisplay->append("----------------------------------------\n");
-    
-    QStringList parts = command.split(" ");
-    QString program = parts.takeFirst();
-    
-    process->start(program, parts);
-}
-
-void MainWindow::onProcessFinished(int exitCode)
-{
-    QString output = process->readAllStandardOutput();
-    QString error = process->readAllStandardError();
-    
-    if (!output.isEmpty()) {
-        outputDisplay->append(output);
-    }
-    
-    if (!error.isEmpty()) {
-        outputDisplay->append("\n[STDERR]:\n" + error);
-    }
-    
-    outputDisplay->append("\n----------------------------------------");
-    outputDisplay->append(QString("\nProcess finished with exit code: %1").arg(exitCode));
-    
-    if (currentButton) {
-        if (exitCode == 0) {
-            setButtonState(currentButton, "idle");
-        } else {
-            setButtonState(currentButton, "error");
-        }
-    }
-}
-
-void MainWindow::onProcessError(QProcess::ProcessError error)
-{
-    QString errorString;
-    switch (error) {
-        case QProcess::FailedToStart:
-            errorString = "Failed to start. The command may not exist or you may lack permissions.";
-            break;
-        case QProcess::Crashed:
-            errorString = "Process crashed.";
-            break;
-        case QProcess::Timedout:
-            errorString = "Process timed out.";
-            break;
-        case QProcess::WriteError:
-            errorString = "Write error occurred.";
-            break;
-        case QProcess::ReadError:
-            errorString = "Read error occurred.";
-            break;
-        case QProcess::UnknownError:
-            errorString = "Unknown error occurred.";
-            break;
-    }
-    
-    outputDisplay->append(QString("\n[ERROR] %1\n").arg(errorString));
-    
-    if (currentButton) {
-        setButtonState(currentButton, "error");
-    }
-}
-
-void MainWindow::setButtonState(QPushButton *button, const QString &state)
-{
-    if (state == "running") {
-        button->setStyleSheet("color: green;");
-    } else if (state == "error") {
-        button->setStyleSheet("color: red;");
-    } else {
         button->setStyleSheet("");
+        outputDisplay->append(QString("\n[%1] Stopped\n").arg(button->text()));
+    } else {
+        QString command = buttonCommandMap[button];
+        QStringList args = buttonArgsMap[button];
+        
+        outputDisplay->append(QString("\n[%1] Starting: %2 %3\n").arg(button->text()).arg(command).arg(args.join(" ")));
+        process->start(command, args);
+        button->setStyleSheet("color: green;");
     }
 }
 
-void MainWindow::startContinuousCommand()
+void MainWindow::onProcessOutput()
 {
-    outputDisplay->clear();
-    outputDisplay->append("Starting ROS launch: roslaunch op3_manager op3_manager.launch\n");
-    outputDisplay->append("----------------------------------------\n");
-    
-    QStringList args;
-    args << "op3_manager" << "op3_manager.launch";
-    
-    continuousProcess->start("roslaunch", args);
-    isContinuousRunning = true;
-    button4->setStyleSheet("color: green;");
-    button4->setText("Stop OP3 Manager");
-}
-
-void MainWindow::stopContinuousCommand()
-{
-    if (continuousProcess->state() == QProcess::Running) {
-        continuousProcess->kill();
-        continuousProcess->waitForFinished();
-    }
-    
-    outputDisplay->append("\n----------------------------------------\n");
-    outputDisplay->append("ROS launch stopped.\n");
-    
-    isContinuousRunning = false;
-    button4->setStyleSheet("");
-    button4->setText("Launch OP3 Manager");
-}
-
-void MainWindow::onContinuousProcessOutput()
-{
-    QString output = continuousProcess->readAllStandardOutput();
-    if (!output.isEmpty()) {
-        outputDisplay->append(output);
+    QProcess *process = qobject_cast<QProcess*>(sender());
+    if (process) {
+        QString output = process->readAllStandardOutput();
+        QString error = process->readAllStandardError();
+        
+        if (!output.isEmpty()) {
+            outputDisplay->append(output);
+        }
+        if (!error.isEmpty()) {
+            outputDisplay->append(error);
+        }
+        
         QTextCursor cursor = outputDisplay->textCursor();
         cursor.movePosition(QTextCursor::End);
         outputDisplay->setTextCursor(cursor);
--- mainwindow.h
+++ mainwindow.h
@@ -5,6 +5,7 @@
 #include <QPushButton>
 #include <QTextEdit>
 #include <QProcess>
+#include <QMap>
 
 class MainWindow : public QMainWindow {
     Q_OBJECT
@@ -14,29 +15,17 @@ public:
     ~MainWindow();
 
 private slots:
-    void onButton1Clicked();
-    void onButton2Clicked();
-    void onButton3Clicked();
-    void onButton4Clicked();
-    void onProcessFinished(int exitCode);
-    void onProcessError(QProcess::ProcessError error);
-    void onContinuousProcessOutput();
+    void onButtonClicked();
+    void onProcessOutput();
 
 private:
-    QPushButton *button1;
-    QPushButton *button2;
-    QPushButton *button3;
-    QPushButton *button4;
+    QMap<QPushButton*, QProcess*> buttonProcessMap;
+    QMap<QPushButton*, QString> buttonCommandMap;
+    QMap<QPushButton*, QStringList> buttonArgsMap;
     QTextEdit *outputDisplay;
-    QProcess *process;
-    QProcess *continuousProcess;
-    QPushButton *currentButton;
-    bool isContinuousRunning;
     
-    void executeCommand(const QString &command, QPushButton *button);
-    void setButtonState(QPushButton *button, const QString &state);
-    void startContinuousCommand();
-    void stopContinuousCommand();
+    void createButton(const QString &label, const QString &command, const QStringList &args);
+    void toggleProcess(QPushButton *button);
 };
 
 #endif
